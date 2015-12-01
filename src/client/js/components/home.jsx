a26d402ef6dd81f08121f6bd9bc4f74751f28134
Andreas Møller <andreasmoller@Andreass-MacBook.local>
2015-12-01 10:56:53 +0000

    added kitten.jpg and updated npm scripts

--- src/client/js/components/home.jsx
+++ src/client/js/components/home.jsx
@@ -1,7 +1,12 @@
 export default (props) => {
-  const title = 'MY MAIN TITLE'
+  const title = 'React Blueprint'
+  const description = 'It even comes with stock kitten photos!!!'
 
   return (
-    <h1>{title}</h1>
+    <div>
+      <h1>{title}</h1>
+      <p>{description}</p>
+      <img src="/images/kitten.jpg"/>
+    </div>
   )
 }
--- src/client/js/main.jsx
+++ src/client/js/main.jsx
@@ -2,4 +2,6 @@ import './globals'
 import 'babel-polyfill'
 import Router from './components/router'
 import ReactDom from 'react-dom'
+
+
 ReactDom.render(<Router/>, document.getElementById('main'))
